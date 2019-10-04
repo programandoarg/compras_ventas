e5e1eeb2a6094996579a294898d3c2ebf54c7098
Martín Rosso <mrosso10@gmail.com>
2019-10-04 10:23:13 -0300

    Bugfix Rails 5

--- lib/compras_ventas/engine.rb
+++ lib/compras_ventas/engine.rb
@@ -1,5 +1,6 @@
 module ComprasVentas
   class Engine < ::Rails::Engine
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    # config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.autoload_paths << File.expand_path("../", __dir__)
   end
 end
